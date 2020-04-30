44a2551bba050e892455e483e8b74d80423aad19
philly4toast <phillipyoun@gmail.com>
2020-04-29 19:11:31 -0700

    New problem 04/29 base saved

--- toyproblem.js
+++ toyproblem.js
@@ -1,93 +1,30 @@
-//toy problem 04/28
 
-// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+// Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
  
 
 // Example 1:
 
-// Input: n = 234
-// Output: 15 
+// Input: num = 14
+// Output: 6
 // Explanation: 
-// Product of digits = 2 * 3 * 4 = 24 
-// Sum of digits = 2 + 3 + 4 = 9 
-// Result = 24 - 9 = 15
+// Step 1) 14 is even; divide by 2 and obtain 7. 
+// Step 2) 7 is odd; subtract 1 and obtain 6.
+// Step 3) 6 is even; divide by 2 and obtain 3. 
+// Step 4) 3 is odd; subtract 1 and obtain 2. 
+// Step 5) 2 is even; divide by 2 and obtain 1. 
+// Step 6) 1 is odd; subtract 1 and obtain 0.
 // Example 2:
 
-// Input: n = 4421
-// Output: 21
+// Input: num = 8
+// Output: 4
 // Explanation: 
-// Product of digits = 4 * 4 * 2 * 1 = 32 
-// Sum of digits = 4 + 4 + 2 + 1 = 11 
-// Result = 32 - 11 = 21
- 
+// Step 1) 8 is even; divide by 2 and obtain 4. 
+// Step 2) 4 is even; divide by 2 and obtain 2. 
+// Step 3) 2 is even; divide by 2 and obtain 1. 
+// Step 4) 1 is odd; subtract 1 and obtain 0.
+// Example 3:
 
-// Constraints:
-
-// 1 <= n <= 10^5
-
-
-//toy problem 04/28
-
-// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
- 
-
-// Example 1:
-
-// Input: n = 234
-// Output: 15 
-// Explanation: 
-// Product of digits = 2 * 3 * 4 = 24 
-// Sum of digits = 2 + 3 + 4 = 9 
-// Result = 24 - 9 = 15
-// Example 2:
-
-// Input: n = 4421
-// Output: 21
-// Explanation: 
-// Product of digits = 4 * 4 * 2 * 1 = 32 
-// Sum of digits = 4 + 4 + 2 + 1 = 11 
-// Result = 32 - 11 = 21
- 
-
-// Constraints:
-
-// 1 <= n <= 10^5˚
-
-var differenceProductSum = (number) => {
-  if (typeof number !== 'number'){
-    return 'not a number'
-  }
-var numString = number.toString();
-var numberArr = numString.split('')
-
-var sum = 0;
-var product = 1;
-for (var i = 0; i < numberArr.length; i++){
-  sum+=Number(numberArr[i]);
-  product*=numberArr[i];
-}
-
-return product - sum;
-
-} 
-
-
-
-
-var assertEqual = (actual, expected)=>{
-  console.log(actual === expected ? 'test passed' : 'FAILED TEST. Expected ' + expected + ', but got ' + actual + '.')
-}
-
-
-
-let example1 = 4321;
-var expected1 = 14;
-var actual1 = differenceProductSum(example1)
-assertEqual(actual1, expected1)
-
-
-let example2 = 'blam';
-var expected2 = 'not a number';
-var actual2 = differenceProductSum(example2)
-assertEqual(actual2, expected2)
-
+// Input: num = 123
+// Output: 12
